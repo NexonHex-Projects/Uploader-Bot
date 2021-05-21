0a0932a6c0336b2ec3117badd3155c35fff87366
廾㠪〤Rɪᴅᴇʀ <84140090+HexRidder@users.noreply.github.com>
2021-05-21 09:06:05 +0530

    Add files via upload

---
+++ translation.py
@@ -0,0 +1,65 @@
+class Translation(object):
+
+    START_TEXT = """<b>Hᴇʟʟᴏ !
+
+ɪ ᴀᴍ ᴀ ᴛᴇʟᴇɢʀᴀᴍ ᴜʀʟ ᴜᴘʟᴏᴀᴅ ʙᴏᴛ!
+
+ᴘʟᴇᴀsᴇ sᴇɴᴅ ᴍᴇ ᴀɴʏ ᴅɪʀᴇᴄᴛ ᴅᴏᴡɴʟᴏᴀᴅ ᴜʀʟ ʟɪɴᴋ, ɪ ᴄᴀɴ ᴜᴘʟᴏᴀᴅ ᴛᴏ ᴛᴇʟᴇɢʀᴀᴍ ᴀs ғɪʟᴇ ᴏʀ ᴠɪᴅᴇᴏ
+
+©️ Nᴇxᴏɴ ᴘʀᴏᴊᴇᴄᴛ<b>"""
+    HELP_USER = """<b>☆ sᴇɴᴅ ᴜʀʟ (ʟɪɴᴋ|ɴᴇᴡ ɴᴀᴍᴇ ᴡɪᴛʜ ᴇxᴛᴇɴsɪᴏɴ).
+
+☆ sᴇɴᴅ ᴄᴜsᴛᴏᴍ ᴛʜᴜᴍʙɴᴀɪʟ (ᴏᴘᴛɪᴏɴᴀʟ). 
+
+☆ sᴇʟᴇᴄᴛ ᴛʜᴇ ʙᴜᴛᴛᴏɴ.
+
+• sᴠɪᴅᴇᴏ - ɢɪᴠᴇ ғɪʟᴇ ᴀs ᴠɪᴅᴇᴏ ᴡɪᴛʜ sᴄʀᴇᴇɴsʜᴏᴛs
+
+• ᴅғɪʟᴇ  - ɢɪᴠᴇ ғɪʟᴇ ᴡɪᴛʜ sᴄʀᴇᴇɴsʜᴏᴛs
+
+• ᴠɪᴅᴇᴏ  - ɢɪᴠᴇ ғɪʟᴇ ᴀs ᴠɪᴅᴇᴏ ᴡɪᴛʜᴏᴜᴛ sᴄʀᴇᴇɴsʜᴏᴛs
+
+• ᴅғɪʟᴇ  - ɢɪᴠᴇ ғɪʟᴇ ᴡɪᴛʜᴏᴜᴛ sᴄʀᴇᴇɴsʜᴏᴛs
+
+©️ Nᴇxᴏɴ ᴘʀᴏᴊᴇᴄᴛ<b>"""
+
+    FORMAT_SELECTION = """Select the desired format: <a href='{}'>file size might be approximate</a>
+    
+Send your custum thumbnail if required.
+You can use /deletethumbnail to delete the auto-generated thumbnail."""
+    
+    SET_CUSTOM_USERNAME_PASSWORD = """If you want to download premium videos, provide in the following format:
+URL | newfilename | username | password"""
+
+
+    UPGRADE_TEXT = "<b>👉 Create own Clone Bot.. </b>  \n\n<a href='https://github.com/Shon-Tg-hub/URLuploader-With-Hotstar'>Click here, Fork and deploy!!</a>"
+    
+    DOWNLOAD_START = "Trying to download your file..."
+    
+    UPLOAD_START = "Uploading now.."
+    
+    AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "Downloaded in {} seconds. \n\nUploaded in {} seconds."
+
+    RCHD_TG_API_LIMIT = "Downloaded in {} seconds.\nDetected File Size: {}\nSorry. But, I cannot upload files greater than 1.95GB due to Telegram API limitations."
+
+    SAVED_CUSTOM_THUMB_NAIL = "Custom thumbnail saved. This will be permanent.\n\nUse /deletethumbnail to clear it."
+
+    DEL_ETED_CUSTOM_THUMB_NAIL = "Custom thumbnail cleared succesfully."
+
+    CUSTOM_CAPTION_UL_FILE = " "
+
+    SLOW_URL_DECED = "Gosh that seems to be a very slow URL. Since you were screwing my home, I am in no mood to download this file. Meanwhile, why don't you try this:==> https://shrtz.me/PtsVnf6 and get me a fast URL so that I can upload to Telegram, without me slowing down for other users."
+
+    NO_VOID_FORMAT_FOUND = "ERROR...\n<b>YouTubeDL</b> said: {}"
+    
+    SHOW_THUMB = "Use /deletethumbnail to clear this thumbnail."
+    
+    NO_THUMB = "No saved thumbnails Found!!\n\nSend an image to save it as your thumbnail permanently."    
+    
+    ABOUT_TEXT = """<b>▪ Jᴜsᴛ ᴀɴ ᴜʀʟ ᴜᴘʟᴏᴀᴅᴇʀ Bᴏᴛ Mᴀᴅᴇ Wɪᴛʜ Pʏᴛʜᴏɴ Lᴀɴɢᴀᴜɢᴇ ᴀɴᴅ Pʏʀᴏɢʀᴀᴍ Lɪʙʀᴀʀʏ...
+
+▪ Sᴏᴜᴄᴇ Cᴏᴅᴇ :- Cᴜʀʀᴇɴᴛʟʏ Uɴᴜᴠᴀɪᴠᴀʙʟᴇ
+
+▪ Fᴏʟʟᴏᴡ Mʏ Dᴇᴠ Oɴ Gɪᴛʜᴜʙ :- [Fᴏʟʟᴏᴡ](https://github.com/Shon-TG-Hub)
+
+▪ Oᴛʜᴇʀ Pʀᴏᴊᴇᴄᴛ's Oғ Nᴇxᴏɴ :- [NᴇxᴏɴPʀᴏᴊᴇᴄᴛs](Https://telegram.me/NexonHex)<b>"""
